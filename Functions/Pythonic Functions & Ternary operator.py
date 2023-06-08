27332d9c5f64200e073693985f241f627ce2da5f
Mohamed Ridha <134459251+hikodz@users.noreply.github.com>
2023-06-08 14:21:53 +0100

    Update Pythonic Functions & Ternary operator.py

--- Functions/Pythonic Functions & Ternary operator.py
+++ Functions/Pythonic Functions & Ternary operator.py
@@ -53,3 +53,46 @@ print(is_div_by4(5))
 print(is_div_by4(6))
 print(is_div_by4(5, 2))
 print(is_div_by4(5, 5))
+
+
+#Section 12: Python Functions, Files & Regex : 
+# Lesson 7 - Pythonic Functions & Ternary operator file 1 : exersice 2  : 
+def check_type_number(*number):
+    
+    return ["Even" if num %2==0 else "Odd" for num in number]
+print(check_type_number(5))    # output:  ['Odd']
+#############################################################################
+#Lesson 7 - Pythonic Functions & Ternary operator file 1 : exersice 3 : 
+def is_prime(number):
+    check_prime = 2
+    if (number ==1) or (number ==2):
+        return "Prime"
+    while check_prime < number:
+        if number % check_prime ==0:
+            return "Not Prime"
+        check_prime += 1
+    return "Prime"
+
+
+print(is_prime(10))  # output: Not Prime
+print(is_prime(11))  # output: Prime
+print(is_prime(12))  # output: Not Prime
+print(is_prime(13))  # output: Prime
+#############################################################################
+#Lesson 7 - Pythonic Functions & Ternary operator file 1 : exersice 4 : 
+def convert_unit(number, unit):
+    # convert unit basic math:
+    convert_to_miles = number * 0.6214
+    convert_to_km = number / 0.6214
+    # convert unit for google :
+    convert_to_F = (number * 9/5) + 32
+    convert_to_C = (number - 32) * 5/9 
+#---------------------------------------------
+    if (unit.lower() == "miles") or (unit.lower() == "km"):
+        return f'{number} Km is {convert_to_miles:.2f} Miles' if (unit.lower() == "km") else f'{number} Miles is {convert_to_km:.2f} Km'
+    
+    return f'{number} Celsius is {convert_to_F:.2f} Farhrenheit' if unit.lower()== 'celsius' else f'{number} Farhrenheit is {convert_to_C:.2f} Celsiust'
+print(convert_unit(100, "Km"))         # output: 100 Km is 62.14 Miles
+print(convert_unit(100, "Miles"))      # output: 100 Miles is 160.93 Km
+print(convert_unit(30, "Celsius"))     # output: 30 Celsius is 86.00 Farhrenheit
+print(convert_unit(86, "Farhrenheit")) # output: 86 Farhrenheit is 30.00 Celsiust
