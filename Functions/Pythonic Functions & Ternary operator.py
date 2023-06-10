805fa346a4c0fd02f28da6077f6f371db081f7f8
Mohamed Ridha <134459251+hikodz@users.noreply.github.com>
2023-06-10 11:56:59 +0100

    Update Pythonic Functions & Ternary operator.py

--- Functions/Pythonic Functions & Ternary operator.py
+++ Functions/Pythonic Functions & Ternary operator.py
@@ -96,3 +96,71 @@ print(convert_unit(100, "Km"))         # output: 100 Km is 62.14 Miles
 print(convert_unit(100, "Miles"))      # output: 100 Miles is 160.93 Km
 print(convert_unit(30, "Celsius"))     # output: 30 Celsius is 86.00 Farhrenheit
 print(convert_unit(86, "Farhrenheit")) # output: 86 Farhrenheit is 30.00 Celsiust
+#####################################################################################
+#Section 12: Python Functions, Files & Regex : 
+# Lesson 7 - Pythonic Functions & Ternary operator file 2 : exersice 1  : 
+# Complete this code to make special_sum function using kwargs (keyword arguments):
+def special_sum(numbers, **kwargs):
+    print(kwargs) # this code is a hint to see what is inside kwargs
+    if kwargs.get("only_even") or kwargs.get("only_odd") :
+        return sum([number for number in numbers if number % 2 == 0])
+    elif kwargs.get("only_odd"):
+        return sum([number for number in numbers if number % 2 != 0])
+    elif kwargs.get("only_positive"):
+        return sum([number for number in numbers if number > 0])
+    elif kwargs.get("only_negative"):
+        return sum([number for number in numbers if number < 0])
+    elif kwargs.get("only_positive_even"):
+        return sum([number for number in numbers if number % 2 == 0 and number > 0])
+    elif kwargs.get("only_positive_odd"):
+        return sum([number for number in numbers if number % 2 != 0 and number > 0])
+    elif kwargs.get("only_negative_even"):
+        return sum([number for number in numbers if number % 2 == 0 and number < 0])
+    elif kwargs.get("only_negative_odd"):
+        return sum([number for number in numbers if number % 2 != 0 and number < 0])
+    return sum(numbers)
+
+numbers = [1, -2, -3, 4, -5, -6, -7, 8, 9, -10, 11, 12, 13, 14, -15, -16, 17, 18, 19, 20]
+print(f"Sum of all numbers: {special_sum(numbers)}")
+print(f"Total even numbers: {special_sum(numbers, only_even=True)}")
+print(f"Total odd numbers: {special_sum(numbers, only_odd=True)}")
+print(f"Total positive numbers: {special_sum(numbers,only_positive=True)}")
+print(f"Total negative numbers: {special_sum(numbers,only_negative=True)}")
+print(f"Total positive even numbers: {special_sum(numbers,only_positive_even=True)}")
+print(f"Total positive odd numbers: {special_sum(numbers,only_positive_odd=True)}")
+print(f"Total negative even numbers: {special_sum(numbers,only_negative_even=True)}")
+print(f"Total negative odd numbers: {special_sum(numbers,only_negative_odd=True)}")
+#####################################################################################
+# Lesson 7 - Pythonic Functions & Ternary operator file 2 : exersice 2  : 
+# Complete this code to make print_grades function that take student name
+# and his grades like the following:
+def print_grades(name, **kwargs): 
+    print(f'{name.title()} is grades:\n{"-"*20}')
+    for sub, grade in kwargs.items():
+        print(f'{sub:10}: {grade}')
+
+print_grades("Mohamed Hamed", math=90, english=80, arabic=100, physics=95, chemistry=85)
+print_grades("Ahmed Khaled", math=80, english=70, arabic=90, physics=85, chemistry=75)
+
+#####################################################################################
+# Lesson 7 - Pythonic Functions & Ternary operator file 2 : exersice 3  : 
+# Make a function that generate a random password with the following requirements:
+from string import punctuation, ascii_lowercase, ascii_uppercase, digits
+from random import choice, shuffle, sample
+def randomPassword(lenth=8, complex=False):
+    passwordUser = []
+    if lenth < 8:
+        print('The number of password content will be very low')
+    elif complex :
+        passwordUser.extend(sample(punctuation, (lenth//2)+1))
+        while len(passwordUser) < lenth :
+            passwordUser.extend([choice(ascii_lowercase), choice(ascii_uppercase), choice(digits)])
+    elif complex == False :
+        while len(passwordUser) < lenth:
+            passwordUser.extend([choice(ascii_lowercase), choice(ascii_uppercase), choice(digits), choice(punctuation)])
+    while len(passwordUser) > lenth:
+        passwordUser.pop()
+    shuffle(passwordUser)
+    passwordUser = ''.join(passwordUser)
+    return passwordUser
+print(randomPassword(10, complex=True))
