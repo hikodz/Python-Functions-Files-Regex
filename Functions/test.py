59b155e2885635bd311f8749cc39c9f13ff336ec
Mohamed Ridha <134459251+hikodz@users.noreply.github.com>
2023-06-10 05:16:17 +0100

    Update test.py

--- Functions/test.py
+++ Functions/test.py
@@ -1,6 +1,44 @@
-def reverse_word_4(word_4):
-    reverse_chr = [word_4[i] for i in range(len(word_4)-1, -1, -1)]
-    use_join = "".join(reverse_chr)
-    print(use_join)    
-input_word_4 = input("Enter your text: ")
-reverse_word_4(input_word_4)
+def random_password(lenth=8, complex=False):
+    import string
+    import random
+    password_user = ''
+    num_lower = 0
+    num_upper = 0
+    num_digits = 0
+    if lenth < 8:
+        print('The number of password content will be very low')
+    elif complex :
+        while num_lower<((lenth/2) // 3):
+            password_user+=random.choice(string.ascii_lowercase)
+            num_lower+=1
+        while num_upper<((lenth/2) // 3):
+            password_user+=random.choice(string.ascii_uppercase)
+            num_upper+=1
+        while num_digits<((lenth/2) // 3):
+            password_user+=random.choice(string.digits)
+            num_digits+=1
+        while len(password_user)<lenth:
+            password_user+=random.choice(string.punctuation)
+        password_list = list(password_user)
+        random.shuffle(password_list)
+        password_user = ''.join(password_list)
+        return password_user
+    elif complex==False :
+        while num_lower<(lenth // 4):
+            password_user+=random.choice(string.ascii_lowercase)
+            num_lower+=1
+        while num_upper<(lenth // 4):
+            password_user+=random.choice(string.ascii_uppercase)
+            num_upper+=1
+        while num_digits<(lenth // 4):
+            password_user+=random.choice(string.digits)
+            num_digits+=1
+        while len(password_user)<lenth:
+            password_user+=random.choice(string.punctuation)
+        password_list = list(password_user)
+        random.shuffle(password_list)
+        password_user = ''.join(password_list)
+        return password_user
+
+
+print(random_password(20, complex=True))
